77b0b71c3d93cee788591a003048523c09d430e6
Ryan Maleki <ryan@maleki.email>
2023-04-05 11:44:54 -0700

    remove dupe

--- string/in_memory_database_test.go
+++ string/in_memory_database_test.go
@@ -13,8 +13,8 @@ func TestInMemoryDictionary(t *testing.T) {
 		{"EXISTS A\nSET A 1\nGET A\nEXISTS A\nUNSET A\nGET A\nEXISTS A", "false 1 true nil false"},
 		{"GET A\nBEGIN\nSET A 1\nGET A\nROLLBACK\nGET A", "nil 1 nil"},
 		{"GET A\nBEGIN\nSET A 1\nGET A\nCOMMIT\nGET A", "nil 1 1"},
+		{"SET A 1\nGET A\nBEGIN\nSET A 2\nGET A\nBEGIN\nUNSET A\nGET A\nCOMMIT\nROLLBACK\nGET A", "1 2 nil 1"},
 		{"SET A 2\nGET A\nBEGIN\nSET A 1\nGET A\nCOMMIT\nGET A\nBEGIN\nSET A 2\nGET A\nROLLBACK\nGET A", "2 1 1 2 1"},
-		{"SET A 1\nGET A\nBEGIN\nSET A 2\nGET A\nBEGIN\nUNSET A\nGET A\nCOMMIT\nGET A\nROLLBACK\nGET A", "1 2 nil nil 1"},
 	}
 
 	for i, test := range tests {
