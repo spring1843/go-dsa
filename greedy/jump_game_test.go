10dc5d4fb95212f61c1574aaa9b5d855062e2a7d
Ryan Maleki <spring1843@users.noreply.github.com>
2023-04-05 19:22:13 -0700

    add jump game greedy problem (#41)

---
+++ greedy/jump_game_test.go
@@ -0,0 +1,25 @@
+package greedy
+
+import (
+	"testing"
+)
+
+func TestJumpGame(t *testing.T) {
+	var tests = []struct {
+		jumps          []int
+		canReachTheEnd bool
+	}{
+		{[]int{}, true},
+		{[]int{1}, true},
+		{[]int{1, 0, 1}, false},
+		{[]int{3, 1, 0, 0, 1}, false},
+		{[]int{2, 3, 1, 1, 4}, true},
+		{[]int{2, 3, 1, 1, 2, 0, 1}, true},
+	}
+
+	for i, test := range tests {
+		if got := JumpGame(test.jumps); got != test.canReachTheEnd {
+			t.Fatalf("Failed test case #%d. Want %t got %t", i, test.canReachTheEnd, got)
+		}
+	}
+}
