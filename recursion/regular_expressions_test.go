cca9c622ab217e7d360bbb43dcd502c063afb4ac
Ryan Maleki <ryan@maleki.email>
2023-06-11 12:37:02 -0700

    more test cases

--- recursion/regular_expressions_test.go
+++ recursion/regular_expressions_test.go
@@ -10,6 +10,8 @@ func TestRegularExpressions(t *testing.T) {
 		match          bool
 	}{
 		{"", "", true},
+		{"", ".", true},
+		{"", "*", true},
 		{"a", "", false},
 		{"a", ".", true},
 		{"a", "*", true},
@@ -19,6 +21,9 @@ func TestRegularExpressions(t *testing.T) {
 		{"ad", ".d", true},
 		{"ad", "*d", true},
 		{"abd", "*d", true},
+		{"abdef", "*d", true},
+		{"abdef", "*d*", true},
+		{"abdef", "d*", true},
 	}
 
 	for i, test := range tests {
