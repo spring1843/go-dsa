07f6c22d4c0a9cac51b83b289590ad5657b0ec34
Ryan Maleki <ryan@maleki.email>
2023-06-11 13:26:14 -0700

    add more test cases and coverage

--- linkedlist/join_sorted_lists.go
+++ linkedlist/join_sorted_lists.go
@@ -6,24 +6,24 @@ func JoinTwoSortedLinkedLists(l1, l2 *Node) *Node {
 	last := head
 	for l1 != nil && l2 != nil {
 		if l1.Val <= l2.Val {
-			last, l1 = moveTolast(last, l1)
+			last, l1 = moveToLast(last, l1)
 		} else {
-			last, l2 = moveTolast(last, l2)
+			last, l2 = moveToLast(last, l2)
 		}
 	}
 
 	for l1 != nil {
-		last, l1 = moveTolast(last, l1)
+		last, l1 = moveToLast(last, l1)
 	}
 
 	for l2 != nil {
-		last, l2 = moveTolast(last, l2)
+		last, l2 = moveToLast(last, l2)
 	}
 
 	return head.Next
 }
 
-func moveTolast(last, node *Node) (*Node, *Node) {
+func moveToLast(last, node *Node) (*Node, *Node) {
 	last.Next = node
 	last = last.Next
 	node = node.Next
