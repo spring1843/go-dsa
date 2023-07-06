750f475bc26f069224adca46e2dc3d15c5be4ffc
Ryan Maleki <spring1843@users.noreply.github.com>
2023-07-06 00:53:44 -0700

    simplify solutions (#73)

--- backtracking/permutations.go
+++ backtracking/permutations.go
@@ -2,22 +2,20 @@ package backtracking
 
 // Permutations intakes a list of numbers and returns all possible permutations of their orders
 // For example for {1,2} it would return {1,2}, {2,1}.
-func Permutations(num []int) [][]int {
+func Permutations(input []int) [][]int {
 	permutations := make([][]int, 0)
-	permutationsRecursive(&num, 0, len(num)-1, &permutations)
+	permutationsRecursive(input, 0, len(input)-1, &permutations)
 	return permutations
 }
 
-func permutationsRecursive(nums *[]int, start, end int, retSlice *[][]int) {
+func permutationsRecursive(input []int, start, end int, permutations *[][]int) {
 	if start == end {
-		copyNums := []int{}
-		copyNums = append(copyNums, *nums...)
-		*retSlice = append(*retSlice, copyNums)
+		*permutations = append(*permutations, append([]int{}, input...))
 		return
 	}
 	for i := start; i <= end; i++ {
-		(*nums)[i], (*nums)[start] = (*nums)[start], (*nums)[i]
-		permutationsRecursive(nums, start+1, end, retSlice)
-		(*nums)[i], (*nums)[start] = (*nums)[start], (*nums)[i]
+		(input)[i], (input)[start] = (input)[start], (input)[i]
+		permutationsRecursive(input, start+1, end, permutations)
+		(input)[i], (input)[start] = (input)[start], (input)[i]
 	}
 }
