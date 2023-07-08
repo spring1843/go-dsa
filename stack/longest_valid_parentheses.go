2c47b9837ddd67b08eb23afe1204c6f3fe0dd12b
Ryan Maleki <spring1843@users.noreply.github.com>
2023-07-07 22:44:34 -0700

    add breaking test case and fix for longest valid parentheses (#86)

--- stack/longest_valid_parentheses.go
+++ stack/longest_valid_parentheses.go
@@ -1,31 +1,38 @@
 package stack
 
-import "container/list"
+var intStack []int
 
 // LongestValidParentheses solves the problem in O(n) time and O(n) space.
 func LongestValidParentheses(input string) int {
-	stack := list.New()
+	intStack = make([]int, 0)
+	intStackPush(-1)
 
 	longest := 0
-
-	tmp := 0
-	for _, char := range input {
+	for i, char := range input {
 		if char == '(' {
-			stack.PushBack(char)
+			intStackPush(i)
 			continue
 		}
-
-		if stack.Len() == 0 {
-			tmp = 0
+		intStackPop()
+		if len(intStack) == 0 {
+			intStackPush(i)
 			continue
 		}
-
-		stack.Remove(stack.Front())
-		tmp += 2
-		if tmp > longest {
-			longest = tmp
-		}
+		longest = max(longest, i-intStackPeek())
 	}
-
 	return longest
+}
+
+func intStackPush(a int) {
+	intStack = append(intStack, a)
+}
+
+func intStackPop() int {
+	tmp := intStack[len(intStack)-1]
+	intStack = intStack[:len(intStack)-1]
+	return tmp
+}
+
+func intStackPeek() int {
+	return intStack[len(intStack)-1]
 }
