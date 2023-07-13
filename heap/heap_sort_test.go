50529e5572c834b2ed192f321b17c7c1e9a5c8f1
Ryan Maleki <ryan@maleki.email>
2023-07-13 13:28:35 -0700

    rename so it does not stutter

--- heap/heap_sort_test.go
+++ heap/heap_sort_test.go
@@ -7,13 +7,13 @@ import (
 )
 
 /*
-TestHeapSort tests solution(s) with the following signature and problem description:
+TestSort tests solution(s) with the following signature and problem description:
 
 	func HeapSort(list []int) []int
 
 Given a list of integers like {3,1,2}, return a sorted set like {1,2,3} using Heap Sort.
 */
-func TestHeapSort(t *testing.T) {
+func TestSort(t *testing.T) {
 	tests := []struct {
 		list   []int
 		sorted []int
@@ -28,7 +28,7 @@ func TestHeapSort(t *testing.T) {
 	}
 
 	for i, test := range tests {
-		if got := HeapSort(test.list); !reflect.DeepEqual(got, test.sorted) {
+		if got := Sort(test.list); !reflect.DeepEqual(got, test.sorted) {
 			t.Fatalf("Failed test case #%d. Want %v got %v", i, test.sorted, got)
 		}
 	}
