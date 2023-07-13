ff4a23f1d41ce8f5f5c16e09c871a3b90203e232
Ryan Maleki <ryan@maleki.email>
2023-07-13 01:13:41 -0700

    add two files

---
+++ .github/scripts/sections.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+declare -a sections=(
+  ./array
+  ./strings
+  ./linkedlist
+  ./stack
+  ./queue
+  ./hashtable
+  ./tree
+  ./heap
+  ./recursion
+  ./dnc
+  ./bit
+  ./backtracking
+  ./graph
+  ./greedy
+  ./dp
+)
