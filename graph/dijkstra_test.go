b7f0ab29d01a6880d41f211cd35cd1525903b808
Ryan Maleki <ryan@maleki.email>
2023-07-21 17:40:08 -0700

    shorter function length

--- graph/dijkstra_test.go
+++ graph/dijkstra_test.go
@@ -4,6 +4,11 @@ import (
 	"testing"
 )
 
+type dijkstraTestCase struct {
+	graph     []*dijkstraVertex
+	distances []int
+}
+
 /*
 TestDijkstra tests solution(s) with the following signature and problem description:
 
@@ -32,57 +37,49 @@ field of the node.
 For the above graph, the shortest paths for each node are {0, 1, 3, 5, 8}.
 */
 func TestDijkstra(t *testing.T) {
-	tests := []struct {
-		graph     []*dijkstraVertex
-		distances []int
-	}{
+	tests := []*dijkstraTestCase{
 		{
-			graph: []*dijkstraVertex{
-				{val: 0},
-				{val: 1},
-			},
+			graph:     newGraph(2),
 			distances: []int{0, 10},
 		},
 		{
-			graph: []*dijkstraVertex{
-				{val: 0},
-				{val: 1},
-				{val: 2},
-				{val: 3},
-				{val: 4},
-			},
+			graph:     newGraph(5),
 			distances: []int{0, 1, 3, 5, 8},
 		},
 		{
-			graph: []*dijkstraVertex{
-				{val: 0},
-				{val: 1},
-				{val: 2},
-				{val: 3},
-			},
+			graph:     newGraph(4),
 			distances: []int{0, 5, 3, 10},
 		},
 		{
-			graph: []*dijkstraVertex{
-				{val: 0},
-				{val: 1},
-				{val: 2},
-			},
+			graph:     newGraph(3),
 			distances: []int{0, 3, 5},
 		},
 		{
-			graph: []*dijkstraVertex{
-				{val: 0},
-				{val: 1},
-				{val: 2},
-				{val: 3},
-				{val: 4},
-				{val: 5},
-			},
+			graph:     newGraph(6),
 			distances: []int{0, 1, 3, 4, 6, 10},
 		},
 	}
 
+	addEdges(tests)
+
+	for i, test := range tests {
+		Dijkstra(test.graph, test.graph[0])
+		for j, v := range test.graph {
+			if v.distance != test.distances[j] {
+				t.Errorf("Test Case %d, Vertex %d distance: got %d, expected %d", i, v.val, v.distance, test.distances[j])
+			}
+		}
+	}
+}
+func newGraph(edges int) []*dijkstraVertex {
+	graph := make([]*dijkstraVertex, edges)
+	for i := 0; i < edges; i++ {
+		graph[i] = &dijkstraVertex{val: i}
+	}
+	return graph
+}
+
+func addEdges(tests []*dijkstraTestCase) {
 	tests[0].graph[0].edges = []*weightedEdge{{edge: tests[0].graph[1], weight: 10}}
 
 	tests[1].graph[0].edges = []*weightedEdge{{edge: tests[1].graph[1], weight: 1}, {edge: tests[1].graph[2], weight: 3}}
@@ -102,13 +99,4 @@ func TestDijkstra(t *testing.T) {
 	tests[4].graph[2].edges = []*weightedEdge{{edge: tests[4].graph[3], weight: 1}, {edge: tests[4].graph[4], weight: 3}}
 	tests[4].graph[3].edges = []*weightedEdge{{edge: tests[4].graph[4], weight: 2}, {edge: tests[4].graph[1], weight: 1}}
 	tests[4].graph[4].edges = []*weightedEdge{{edge: tests[4].graph[5], weight: 4}}
-
-	for i, test := range tests {
-		Dijkstra(test.graph, test.graph[0])
-		for j, v := range test.graph {
-			if v.distance != test.distances[j] {
-				t.Errorf("Test Case %d, Vertex %d distance: got %d, expected %d", i, v.val, v.distance, test.distances[j])
-			}
-		}
-	}
 }
