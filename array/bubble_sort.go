136eaab43df271d8818616f87f2c9fffb2065187
Shrinivas Pai Kane <shrinivaskane@gmail.com>
2023-12-17 16:18:41 -0500

    Optimized code for bubble sort

--- array/bubble_sort.go
+++ array/bubble_sort.go
@@ -2,11 +2,20 @@ package array
 
 // BubbleSort solves the problem in O(n^2) time and O(1) space.
 func BubbleSort(input []int) {
-	for i := range input {
-		for j := range input {
-			if input[i] < input[j] {
-				input[i], input[j] = input[j], input[i]
+	n := len(input)
+	for i := 0; i < n-1; i++ {
+		// flag to check if there were any swaps made
+		swapped := false
+		// loop can run until n-i-1 since last i elements are already sorted in ith pass
+		for j := 0; j < n-i-1; j++ {
+			if input[j] > input[j+1] {
+				input[j], input[j+1] = input[j+1], input[j]
+				swapped = true
 			}
+		}
+		if !swapped {
+			// if no swaps were made, array is already sorted
+			break
 		}
 	}
 }
