68f1be5c5c41097c1fe157bc0da19f9e2dea312b
Shrinivas Pai Kane <shrinivaskane@gmail.com>
2023-12-17 17:45:33 -0500

    Added empty list and invalid index check

--- array/find_duplicate_in_array.go
+++ array/find_duplicate_in_array.go
@@ -2,8 +2,16 @@ package array
 
 // FindDuplicate solves the problem in O(n) time and O(1) space.
 func FindDuplicate(list []int) int {
+	if len(list) == 0 {
+		return -1
+	}
+	
 	for _, item := range list {
 		itemIndex := item - 1
+		// Check for Invalid index if any, return -1 indicating an error.
+		if itemIndex < 0 || itemIndex >= len(list) {
+			return -1
+		}
 		if list[itemIndex] < 0 {
 			return list[itemIndex] * -1
 		}
