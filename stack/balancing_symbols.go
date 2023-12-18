2bb24275e9cefd13a6a699fd8ab141ee5999d55c
Shrinivas Pai Kane <shrinivaskane@gmail.com>
2023-12-18 15:35:50 -0500

    Using a lcoalized stack instead of global stack for balancing_symbols.go to prevent issues with concurrency

--- stack/balancing_symbols.go
+++ stack/balancing_symbols.go
@@ -6,42 +6,39 @@ var (
 		'(': ')',
 		'{': '}',
 	}
-
-	// stack retains the opener symbols e.g. [({.
-	stack []rune
 )
 
 // IsExpressionBalanced solves the problem in O(n) time and O(n) space.
 func IsExpressionBalanced(s string) bool {
-	stack = []rune{}
-	if s == "" {
-		return true
-	}
-	for _, c := range s {
+	// stack retains the opener symbols e.g. [({.
+	stack := []rune{} // Localized stack
+
+    for _, c := range s {
 		if _, ok := symbols[c]; ok {
-			push(c)
-			continue
-		}
+            stack = push(stack, c)
+            continue
+        }
 
-		if len(stack) == 0 {
-			return false // More closers than openers
-		}
+        if len(stack) == 0 {
+            return false // More closers than openers
+        }
 
-		lastOpener := pop()
-		if symbols[lastOpener] != c {
-			return false // opener did not match the last closer
-		}
-	}
+        var lastOpener rune
+        stack, lastOpener = pop(stack)
+        if symbols[lastOpener] != c {
+            return false // Opener did not match the last closer
+        }
+    }
 
-	return len(stack) == 0
+    return len(stack) == 0
 }
 
-func push(a rune) {
-	stack = append(stack, a)
+func push(stack []rune, a rune) []rune {
+	return append(stack, a)
 }
 
-func pop() rune {
-	tmp := stack[len(stack)-1]
-	stack = stack[:len(stack)-1]
-	return tmp
+func pop(stack []rune) ([]rune, rune) {
+    tmp := stack[len(stack)-1]
+    stack = stack[:len(stack)-1]
+    return stack, tmp
 }
