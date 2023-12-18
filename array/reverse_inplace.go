1da49f1a6c1ae8cb9cddd676a7d56df5881cf90c
Shrinivas Pai Kane <shrinivaskane@gmail.com>
2023-12-17 19:42:03 -0500

    Added input validation for reverse inplace

--- array/reverse_inplace.go
+++ array/reverse_inplace.go
@@ -2,6 +2,11 @@ package array
 
 // ReverseInPlace solves the problem in O(n) time and O(1) space.
 func ReverseInPlace(list []int, start, end int) {
+	// validation of input
+	if start < 0 || end >= len(list) || start > end {
+		// if indices are invalid, do nothing
+		return
+	}
 	for i := start; i <= start+end/2 && i < end-i+start; i++ {
 		list[i], list[end-i+start] = list[end-i+start], list[i]
 	}
