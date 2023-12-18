a742c80ed77bc55acda860d82f53d3fdce069824
Shrinivas Pai Kane <shrinivaskane@gmail.com>
2023-12-18 18:36:20 -0500

    Used iterative solution to reduce space complexity of the solution

--- recursion/is_palindrome.go
+++ recursion/is_palindrome.go
@@ -2,17 +2,13 @@ package recursion
 
 // IsPalindrome solves the problem in O(n) time and O(n) space.
 func IsPalindrome(s string) bool {
-	if len(s) < 2 {
-		return true
-	}
-
-	if len(s) == 2 {
-		return s[0] == s[1]
-	}
-
-	if s[0] != s[len(s)-1] {
-		return false
-	}
-
-	return IsPalindrome(s[1 : len(s)-1])
+	left, right := 0, len(s)-1
+    for left < right {
+        if s[left] != s[right] {
+            return false
+        }
+        left++
+        right--
+    }
+    return true
 }
