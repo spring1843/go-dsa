092f684b8c24e0d01ceba5486fa8303f39b37d25
Ryan Maleki <ryan@maleki.email>
2024-08-18 22:41:30 -0700

    improve find duplicates problem description and tests

--- array/find_duplicate_in_array.go
+++ array/find_duplicate_in_array.go
@@ -1,11 +1,13 @@
 package array
 
+import "math"
+
 // FindDuplicate solves the problem in O(n) time and O(1) space.
 func FindDuplicate(list []int) int {
 	for _, item := range list {
-		itemIndex := item - 1
+		itemIndex := int(math.Abs(float64(item))) - 1
 		if list[itemIndex] < 0 {
-			return list[itemIndex] * -1
+			return item
 		}
 		list[itemIndex] *= -1
 	}
