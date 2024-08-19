7668a1fe58924c1f50e755eefe91c45494cd8f27
Ryan Maleki <spring1843@users.noreply.github.com>
2024-08-18 22:45:53 -0700

    More efficient bubble sort (#141)

--- array/bubble_sort.go
+++ array/bubble_sort.go
@@ -2,10 +2,13 @@ package array
 
 // BubbleSort solves the problem in O(n^2) time and O(1) space.
 func BubbleSort(input []int) {
-	for i := range input {
-		for j := range input {
-			if input[i] < input[j] {
-				input[i], input[j] = input[j], input[i]
+	swapped := true
+	for swapped {
+		swapped = false
+		for i := 1; i < len(input); i++ {
+			if input[i] < input[i-1] {
+				input[i], input[i-1] = input[i-1], input[i]
+				swapped = true
 			}
 		}
 	}
