692cda3b5fb685df8c3542aca68f4df5573696a7
Ryan Maleki <ryan@maleki.email>
2024-12-28 18:16:29 -0800

    add option to replace package name to main in export, and add new flags for making the links work in the exported doc

--- .github/cmd/pkg/problems/rehearsal.go
+++ .github/cmd/pkg/problems/rehearsal.go
@@ -14,6 +14,8 @@ type rehearsalEntry struct {
 	SolutionFileName string
 }
 
+var rehearsalLinkPattern = regexp.MustCompile(`\* \[([^\]]+)\]\(\.\/([^\)]+)\), \[Solution\]\(\.\/([^\)]+)\)`)
+
 func (r *rehearsalEntry) String() string {
 	return fmt.Sprintf("### %s\n", r.Name)
 }
@@ -29,14 +31,13 @@ newRehearsalEntry parses the rehearsal section of the README.md file that looks 
 func newRehearsalEntry(input string) ([]rehearsalEntry, error) {
 	lines := strings.Split(input, "\n")
 	entries := []rehearsalEntry{}
-	re := regexp.MustCompile(`\* \[([^\]]+)\]\(\.\/([^\)]+)\), \[Solution\]\(\.\/([^\)]+)\)`)
 
 	for _, line := range lines {
 		line = strings.TrimSpace(line)
 		if line == "" || strings.HasPrefix(line, "##") {
 			continue // Skip empty lines and heading lines
 		}
-		matches := re.FindStringSubmatch(line)
+		matches := rehearsalLinkPattern.FindStringSubmatch(line)
 		if len(matches) != 4 {
 			return nil, fmt.Errorf("invalid line format: %s, %d", line, len(matches))
 		}
@@ -52,17 +53,17 @@ func newRehearsalEntry(input string) ([]rehearsalEntry, error) {
 	return entries, nil
 }
 
-func stringRehearsalEntries(dir, section string, entries []rehearsalEntry) string {
+func stringRehearsalEntries(conf *ParseConf, entries []rehearsalEntry) string {
 	output := ""
 	for _, entry := range entries {
 		output += fmt.Sprintf("\n### %s\n", entry.Name)
 
-		testFileContent, err := os.ReadFile(filepath.Join(dir, section, entry.TestFileName))
+		testFileContent, err := goFileFilters(filepath.Join(conf.Dir, conf.Section, entry.TestFileName), conf)
 		if err != nil {
 			output += fmt.Sprintf("Error reading test file: %s\n", err)
 			continue
 		}
-		output += "```GO\n" + string(testFileContent) + "\n```\n"
+		output += "\n```GO\n" + string(testFileContent) + "\n```\n"
 	}
 
 	output += "\n## Rehearsal Solutions\n"
@@ -70,13 +71,29 @@ func stringRehearsalEntries(dir, section string, entries []rehearsalEntry) strin
 	for _, entry := range entries {
 		output += fmt.Sprintf("\n### %s\n", entry.Name)
 
-		testFileContent, err := os.ReadFile(filepath.Join(dir, section, entry.SolutionFileName))
+		testFileContent, err := goFileFilters(filepath.Join(conf.Dir, conf.Section, entry.SolutionFileName), conf)
 		if err != nil {
 			output += fmt.Sprintf("Error reading test file: %s\n", err)
 			continue
 		}
-		output += "```GO\n" + string(testFileContent) + "\n```\n"
+		output += "\n```GO\n" + string(testFileContent) + "\n```\n"
 	}
 
 	return output
+}
+
+func goFileFilters(filePath string, conf *ParseConf) (string, error) {
+	fileContent, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", fmt.Errorf("error reading solution file [%s]: %s", filePath, err)
+	}
+
+	fileContentStr := string(fileContent)
+
+	if conf.ReplacePackageWithMain {
+		re := regexp.MustCompile(`(?m)^package ` + conf.Section + `$`)
+		fileContentStr = re.ReplaceAllString(fileContentStr, "package main")
+	}
+
+	return fileContentStr, nil
 }
