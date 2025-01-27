2564014150dd00fd8ae25d1be8fe3f1f4a1321ef
Ryan Maleki <ryan@maleki.email>
2025-01-26 18:46:11 -0800

    Example comments for the rest of the problems

--- strings/longest_substring_test.go
+++ strings/longest_substring_test.go
@@ -7,7 +7,15 @@ TestLongestSubstrings tests solution(s) with the following signature and problem
 
 	func LongestSubstringOfTwoChars(input string) string
 
-Given a string like "aabbc" return the longest substring of two unique characters like "aabb".
+Given a string return the longest substring of two unique characters.
+
+For example given "aabbc", return "aabb" because it's the longest substring that has two unique
+characters "a" and "b".
+
+Other substrings of "aabc" include:
+
+* "aabbc", contains more than 2 unique characters.
+* "bbc", shorter than "aabb".
 */
 func TestLongestSubstrings(t *testing.T) {
 	tests := []struct {
@@ -21,7 +29,7 @@ func TestLongestSubstrings(t *testing.T) {
 		{"aabbc", "aabb"},
 		{"ada", "ada"},
 		{"dafff", "afff"},
-		{"assdeeeddfffha", "deeedd"},
+		{"abbdeeeddfffha", "deeedd"},
 	}
 
 	for i, test := range tests {
--- strings/roman_numerals_test.go
+++ strings/roman_numerals_test.go
@@ -7,7 +7,24 @@ TestIntToRoman tests solution(s) with the following signature and problem descri
 
 	func IntToRoman(input int) string
 
-Given a positive integer like 1917 return the Roman numeral like MCMXVII.
+Given a positive integer like return the equivalent inRoman numerals:
+
+For example:
+
+* Given 1, return I
+* Given 4, return IV
+* Given 5, return V
+* Given 9, return IX
+* Given 10, return X
+* Given 40, return XL
+* Given 50, return L
+* Given 90, return XC
+* Given 100, return C
+* Given 400, return CD
+* Given 500, return D
+* Given 900, return CM
+* Given 1000, return M
+* Given 1917, return MCMXVII.
 */
 func TestIntToRoman(t *testing.T) {
 	tests := []struct {
